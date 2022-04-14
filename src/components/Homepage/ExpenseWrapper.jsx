d22f18bcd21e3d6dd4651d585c261e626738754e
Nishant Thakur <nishantthakur1357@gmail.com>
2022-04-14 07:12:26 +0530

    Update ExpenseWrapper.jsx

--- src/components/Homepage/ExpenseWrapper.jsx
+++ src/components/Homepage/ExpenseWrapper.jsx
@@ -6,11 +6,13 @@ const ExpenseWrapper = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
+    <>
     <div className={`ExpenseWrapper ${darkMode ? "wrap-dark" : "wrap-light"} `}>
       <ExpenseItemList />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9649316000645471"
      crossorigin="anonymous"></script>
     </div>
+    </>
   );
 };
 
